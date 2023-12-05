fd78710ba50a3cc08fd5b2c4fb172f11a022f1ee
Andy Miles <akmiles@icloud.com>
2023-12-05 12:45:46 -0800

    v0.1 wip

---
+++ app/main.py
@@ -0,0 +1,10 @@
+import app.maths
+
+
+def run():
+    result = app.maths.addup(1, 2)
+    print(f"{result=}")
+
+
+if __name__ == "__main__":
+    run()
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+import app.main
+
+
+def test_main():
+    assert app.main.run()
