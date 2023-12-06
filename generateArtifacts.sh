fcaffb4a0c64a8566c5a13ad5ac23f5610ea95d0
Andy Miles <akmiles@icloud.com>
2023-12-06 11:45:09 -0800

    changed quoting in sh

--- generateArtifacts.sh
+++ generateArtifacts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker buildx build --tag \
-  "$spaceRepo:1.$JB_SPACE_EXECUTION_NUMBER" \
+  "$spaceRepo:1.1" \
   -o type=image --platform=linux/arm64,linux/amd64 --push -f Dockerfile .
