ba139359114f07d770743ac2a08846132cd719e9
Andy Miles <akmiles@icloud.com>
2023-12-06 13:28:47 -0800

    changed quoting in sh

--- .space.kts
+++ .space.kts
@@ -1,23 +1,27 @@
 job("Build and push Docker") {
-    host("Build artifacts and a Docker image") {
-        // generate artifacts required for the image
-        shellScript {
-            content =
-               ./generateArtifacts.sh
-          }
-
+    host("Build and push a Docker image") {
         dockerBuildPush {
-        
-            context = ""
-            file = "Dockerfile"
-            labels["vendor"] = "pqoquickly"
-            // args["HTTP_PROXY"] = "http://10.20.30.1:123"
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
 
-            val spaceRepo = "proqickly.registry.jetbrains.space/p/py_template/py_template"
-            // image tags for 'docker push'
+            // path to Docker context (by default, context is working dir)
+            context = "docker"
+            // path to Dockerfile relative to the project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+            file = "Dockerfile"
+            // build-time variables
+            //args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            labels["vendor"] = "proquickly"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
             tags {
-                +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
-                +"$spaceRepo:latest"
+                // use current job run number as a tag - '0.0.run_number'
+                +"proquickly.registry.jetbrains.space/p/clients/clients/py_template:latest:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
             }
         }
     }
