e408b68d2b4cfa7849d44cbc1cae7c09f6bbc993
Andy Miles <akmiles@icloud.com>
2023-12-06 11:41:49 -0800

    changed quoting in sh

--- generateArtifacts.sh
+++ generateArtifacts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker buildx build --tag \
-  "$spaceRepo:1.0.${$}JB_SPACE_EXECUTION_NUMBER"+"$spaceRepo:latest" \
+  "$spaceRepo:1.0.$JB_SPACE_EXECUTION_NUMBER"+"$spaceRepo:latest" \
   -o type=image --platform=linux/arm64,linux/amd64 --push -f Dockerfile .
